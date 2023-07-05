52f8c840bb5c3b959cf2f4fde9f9984e64918019
Gustavo René <renegustavo42@gmail.com>
2023-07-05 14:22:15 -0300

    feat: configs for deploy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 // also installed nodemon to reload the application after any change
-
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const db = require("./db");
 const routesCategories = require("./routes/categories");
 const routesUsers = require("./routes/users");
 const routesFinances = require("./routes/finances");
 
 const app = express();
+app.use(
+cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Olá, essa é a aplicação Wallet App!');
 });
 
 app.use("/categories", routesCategories);
