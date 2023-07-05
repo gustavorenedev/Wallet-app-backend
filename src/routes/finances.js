c8c98a73c6e238b7d8dfcf0a2f49e28f01ead7d6
Gustavo René <renegustavo42@gmail.com>
2023-07-05 13:58:50 -0300

    ref: update finances

--- src/routes/finances.js
+++ src/routes/finances.js
@@ -121,8 +121,6 @@ router.get("/", async(req, res) => {
         const initDate = new Date(year, month, 1).toISOString();
         const finDate = new Date(year, month + 1, 0).toISOString();
 
-        console.log({ initDate, finDate });
-
         const text = "SELECT fin.title, fin.value, fin.date, fin.user_id, fin.category_id, cat.name FROM finances as fin JOIN categories as cat ON fin.category_id = cat.id WHERE fin.user_id=$1 AND fin.date BETWEEN $2 AND $3 ORDER BY fin.date ASC";
         const values = [userQuery.rows[0].id, initDate, finDate];
         const financesQuery = await db.query(text, values);
